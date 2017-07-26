84145e140de151798caa7ee4df4069f67f1295b9
Vinay <vinaymvp612@gmail.com>
2017-07-27 01:26:34 +0530

    assertion added

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 exports.capitalizeName = function(name){
-    return name.toUpperCase();
+    if(!name){
+        throw Error('Invalid input type', name);
+    }
+    var _name = name.toLowerCase().trim();
+    return _name[0].toUpperCase() + _name.slice(1);
 }
