cba0ec9771a3eda5ee58299a0e7ebec26c680c26
Vinay <vinaymvp612@gmail.com>
2017-07-27 10:38:26 +0530

    email validation added

--- index.js
+++ index.js
@@ -5,3 +5,9 @@ exports.capitalizeName = function(name){
     var _name = name.toLowerCase().trim();
     return _name[0].toUpperCase() + _name.slice(1);
 }
+
+exports.isValidEmail = function(email){
+    var email = email.toLowerCase().trim();
+    var regExPatter = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return regExPatter.test(email) ? true : false;
+}
