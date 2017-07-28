4faf3b1f83e93a37b931b86f4c7a9044ebc9948b
Vinay <vinaymvp612@gmail.com>
2017-07-28 22:33:03 +0530

    Name validation added

--- index.js
+++ index.js
@@ -4,10 +4,15 @@ exports.capitalizeName = function(name){
     }
     var _name = name.toLowerCase().trim();
     return _name[0].toUpperCase() + _name.slice(1);
-}
+};
 
 exports.isValidEmail = function(email){
     var email = email.toLowerCase().trim();
     var regExPatter = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     return regExPatter.test(email) ? true : false;
+};
+
+exports.isValidName = function(name){
+    var regEx = /^[A-Za-z]+$/;
+    return regEx.test(name);
 }
