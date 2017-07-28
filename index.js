3b3c1613816107109532e9dee57f4491bfcce6bb
Vinay <vinaymvp612@gmail.com>
2017-07-28 23:56:01 +0530

    Validation helpers added

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-exports.capitalizeName = function(name){
-    if(!name){
+exports.capitalizeName = capitalizeName;
+exports.isValidEmail = isValidEmail;
+exports.isValidName = isValidName;
+exports.isValidPhoneNumber = isValidPhoneNumber;
+exports.isValidNumber = isValidNumber;
+exports.hasSpecialChar = hasSpecialChar;
+
+
+function capitalizeName(name) {
+    if (!name) {
         throw Error('Invalid input type', name);
     }
     var _name = name.toLowerCase().trim();
     return _name[0].toUpperCase() + _name.slice(1);
-};
+}
 
-exports.isValidEmail = function(email){
+function isValidEmail(email) {
     var email = email.toLowerCase().trim();
     var regExPatter = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     return regExPatter.test(email) ? true : false;
-};
+}
 
-exports.isValidName = function(name){
+function isValidName(name) {
     var regEx = /^[A-Za-z]+$/;
     return regEx.test(name);
+}
+
+function isValidPhoneNumber(phoneNum) {
+    return (phoneNum.length <= 12 && phoneNum.length >= 10) && isValidNumber(phoneNum);
+}
+
+function isValidNumber(number){
+    return /^[0-9]+$/.test(number);
+}
+
+function hasSpecialChar(char){
+    return /[^a-z\d\s]+/gi.test(char);
 }
